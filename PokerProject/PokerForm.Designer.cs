b29f5cc2f1c7c000fa1d4e0b4383fb498b98f112
JoeWilder <94562379+JoeWilder@users.noreply.github.com>
2022-02-20 16:59:11 -0500

    Finished project

---
+++ PokerProject/PokerForm.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace PokerProject
+{
+    partial class PokerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardBox1 = new System.Windows.Forms.PictureBox();
+            this.cardBox2 = new System.Windows.Forms.PictureBox();
+            this.cardBox3 = new System.Windows.Forms.PictureBox();
+            this.cardBox4 = new System.Windows.Forms.PictureBox();
+            this.cardBox5 = new System.Windows.Forms.PictureBox();
+            this.dealCardsButton = new System.Windows.Forms.Button();
+            this.cardBox6 = new System.Windows.Forms.PictureBox();
+            this.cardBox7 = new System.Windows.Forms.PictureBox();
+            this.cardBox8 = new System.Windows.Forms.PictureBox();
+            this.cardBox9 = new System.Windows.Forms.PictureBox();
+            this.cardBox10 = new System.Windows.Forms.PictureBox();
+            this.Hand1Label = new System.Windows.Forms.Label();
+            this.validateHandButton = new System.Windows.Forms.Button();
+            this.Hand2Label = new System.Windows.Forms.Label();
+            this.WinLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardBox1
+            // 
+            this.cardBox1.Location = new System.Drawing.Point(12, 12);
+            this.cardBox1.Name = "cardBox1";
+            this.cardBox1.Size = new System.Drawing.Size(118, 190);
+            this.cardBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox1.TabIndex = 0;
+            this.cardBox1.TabStop = false;
+            // 
+            // cardBox2
+            // 
+            this.cardBox2.Location = new System.Drawing.Point(136, 12);
+            this.cardBox2.Name = "cardBox2";
+            this.cardBox2.Size = new System.Drawing.Size(118, 190);
+            this.cardBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox2.TabIndex = 1;
+            this.cardBox2.TabStop = false;
+            // 
+            // cardBox3
+            // 
+            this.cardBox3.Location = new System.Drawing.Point(260, 12);
+            this.cardBox3.Name = "cardBox3";
+            this.cardBox3.Size = new System.Drawing.Size(118, 190);
+            this.cardBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox3.TabIndex = 2;
+            this.cardBox3.TabStop = false;
+            // 
+            // cardBox4
+            // 
+            this.cardBox4.Location = new System.Drawing.Point(384, 12);
+            this.cardBox4.Name = "cardBox4";
+            this.cardBox4.Size = new System.Drawing.Size(118, 190);
+            this.cardBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox4.TabIndex = 3;
+            this.cardBox4.TabStop = false;
+            // 
+            // cardBox5
+            // 
+            this.cardBox5.Location = new System.Drawing.Point(508, 12);
+            this.cardBox5.Name = "cardBox5";
+            this.cardBox5.Size = new System.Drawing.Size(118, 190);
+            this.cardBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox5.TabIndex = 4;
+            this.cardBox5.TabStop = false;
+            // 
+            // dealCardsButton
+            // 
+            this.dealCardsButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dealCardsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dealCardsButton.Location = new System.Drawing.Point(716, 12);
+            this.dealCardsButton.Name = "dealCardsButton";
+            this.dealCardsButton.Size = new System.Drawing.Size(322, 190);
+            this.dealCardsButton.TabIndex = 5;
+            this.dealCardsButton.Text = "Deal Cards";
+            this.dealCardsButton.UseVisualStyleBackColor = false;
+            this.dealCardsButton.Click += new System.EventHandler(this.dealCardsButton_Click);
+            // 
+            // cardBox6
+            // 
+            this.cardBox6.Location = new System.Drawing.Point(12, 428);
+            this.cardBox6.Name = "cardBox6";
+            this.cardBox6.Size = new System.Drawing.Size(118, 190);
+            this.cardBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox6.TabIndex = 6;
+            this.cardBox6.TabStop = false;
+            // 
+            // cardBox7
+            // 
+            this.cardBox7.Location = new System.Drawing.Point(136, 428);
+            this.cardBox7.Name = "cardBox7";
+            this.cardBox7.Size = new System.Drawing.Size(118, 190);
+            this.cardBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox7.TabIndex = 7;
+            this.cardBox7.TabStop = false;
+            // 
+            // cardBox8
+            // 
+            this.cardBox8.Location = new System.Drawing.Point(260, 428);
+            this.cardBox8.Name = "cardBox8";
+            this.cardBox8.Size = new System.Drawing.Size(118, 190);
+            this.cardBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox8.TabIndex = 8;
+            this.cardBox8.TabStop = false;
+            // 
+            // cardBox9
+            // 
+            this.cardBox9.Location = new System.Drawing.Point(384, 428);
+            this.cardBox9.Name = "cardBox9";
+            this.cardBox9.Size = new System.Drawing.Size(118, 190);
+            this.cardBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox9.TabIndex = 9;
+            this.cardBox9.TabStop = false;
+            // 
+            // cardBox10
+            // 
+            this.cardBox10.Location = new System.Drawing.Point(508, 428);
+            this.cardBox10.Name = "cardBox10";
+            this.cardBox10.Size = new System.Drawing.Size(118, 190);
+            this.cardBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBox10.TabIndex = 10;
+            this.cardBox10.TabStop = false;
+            // 
+            // Hand1Label
+            // 
+            this.Hand1Label.AutoSize = true;
+            this.Hand1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hand1Label.Location = new System.Drawing.Point(285, 218);
+            this.Hand1Label.Name = "Hand1Label";
+            this.Hand1Label.Size = new System.Drawing.Size(0, 25);
+            this.Hand1Label.TabIndex = 11;
+            // 
+            // validateHandButton
+            // 
+            this.validateHandButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.validateHandButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validateHandButton.Location = new System.Drawing.Point(716, 428);
+            this.validateHandButton.Name = "validateHandButton";
+            this.validateHandButton.Size = new System.Drawing.Size(322, 190);
+            this.validateHandButton.TabIndex = 12;
+            this.validateHandButton.Text = "Validate Cards";
+            this.validateHandButton.UseVisualStyleBackColor = false;
+            this.validateHandButton.Click += new System.EventHandler(this.validateHandButton_Click);
+            // 
+            // Hand2Label
+            // 
+            this.Hand2Label.AutoSize = true;
+            this.Hand2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hand2Label.Location = new System.Drawing.Point(285, 398);
+            this.Hand2Label.Name = "Hand2Label";
+            this.Hand2Label.Size = new System.Drawing.Size(0, 25);
+            this.Hand2Label.TabIndex = 13;
+            // 
+            // WinLabel
+            // 
+            this.WinLabel.AutoSize = true;
+            this.WinLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WinLabel.Location = new System.Drawing.Point(285, 308);
+            this.WinLabel.Name = "WinLabel";
+            this.WinLabel.Size = new System.Drawing.Size(0, 25);
+            this.WinLabel.TabIndex = 14;
+            // 
+            // PokerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1094, 630);
+            this.Controls.Add(this.WinLabel);
+            this.Controls.Add(this.Hand2Label);
+            this.Controls.Add(this.validateHandButton);
+            this.Controls.Add(this.Hand1Label);
+            this.Controls.Add(this.cardBox10);
+            this.Controls.Add(this.cardBox9);
+            this.Controls.Add(this.cardBox8);
+            this.Controls.Add(this.cardBox7);
+            this.Controls.Add(this.cardBox6);
+            this.Controls.Add(this.dealCardsButton);
+            this.Controls.Add(this.cardBox5);
+            this.Controls.Add(this.cardBox4);
+            this.Controls.Add(this.cardBox3);
+            this.Controls.Add(this.cardBox2);
+            this.Controls.Add(this.cardBox1);
+            this.Name = "PokerForm";
+            this.Text = "Poker";
+            this.Load += new System.EventHandler(this.PokerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardBox1;
+        private System.Windows.Forms.PictureBox cardBox2;
+        private System.Windows.Forms.PictureBox cardBox3;
+        private System.Windows.Forms.PictureBox cardBox4;
+        private System.Windows.Forms.PictureBox cardBox5;
+        private System.Windows.Forms.Button dealCardsButton;
+        private System.Windows.Forms.PictureBox cardBox6;
+        private System.Windows.Forms.PictureBox cardBox7;
+        private System.Windows.Forms.PictureBox cardBox8;
+        private System.Windows.Forms.PictureBox cardBox9;
+        private System.Windows.Forms.PictureBox cardBox10;
+        private System.Windows.Forms.Label Hand1Label;
+        private System.Windows.Forms.Button validateHandButton;
+        private System.Windows.Forms.Label Hand2Label;
+        private System.Windows.Forms.Label WinLabel;
+    }
+}
+
---
+++ PokerProject/PokerHand.cs
@@ -0,0 +1,277 @@
+﻿/*******************************************
+Program:  PokerHand.cs
+Purpose: To store data and evaluate a hand of 5 cards
+Author:  Joe Wilder, Ty Rivera, Colin Capelo, Alex Kennedy
+Date: Feb 9th, 2022
+********************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerProject
+{
+    class PokerHand
+    {
+
+        Card[] hand = new Card[5]; // store cards in an array
+        int handValue = 0; // how valuable the hand is
+
+
+        /* Fill hand array using a constructor */
+        public PokerHand(Card card1, Card card2, Card card3, Card card4, Card card5)
+        {
+            hand[0] = card1;
+            hand[1] = card2;
+            hand[2] = card3;
+            hand[3] = card4;
+            hand[4] = card5;
+        }
+
+        /* Sort the hand from lowest rank to highest rank */
+        public void SortHandByRank()
+        {
+            int i, j, min_j;
+            for (i = 0; i < hand.Length; i++)
+            {
+                min_j = i;
+                for (j = i+1; j < hand.Length; j++)
+                {
+                    if (hand[j].GetValue() < hand[min_j].GetValue())
+                    {
+                        min_j = j;
+                    }
+                }
+                Card temp = hand[i];
+                hand[i] = hand[min_j];
+                hand[min_j] = temp;
+            }
+        }
+
+        /* Group suits together */
+        public void SortHandBySuit()
+        {
+            int i, j, min_j;
+            for (i = 0; i < hand.Length; i++)
+            {
+                min_j = i;
+                for (j = i + 1; j < hand.Length; j++)
+                {
+                    if (hand[j].GetSuitID() < hand[min_j].GetSuitID())
+                    {
+                        min_j = j;
+                    }
+                }
+                Card temp = hand[i];
+                hand[i] = hand[min_j];
+                hand[min_j] = temp;
+            }
+        }
+
+
+        /* Check if hand has 5 cards with the same suit */
+        public Boolean Flush()
+        {
+            SortHandBySuit();
+            if (hand[0].GetSuit() == hand[4].GetSuit())
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /* Check if hand has four cards with the same rank */
+        public Boolean FourOfAKind()
+        {
+            SortHandByRank();
+            if (hand[0].GetRank() == hand[1].GetRank() && hand[1].GetRank() == hand[2].GetRank() &&
+                hand[2].GetRank() == hand[3].GetRank())
+            {
+                return true;
+            }
+            if (hand[1].GetRank() == hand[2].GetRank() &&
+                hand[2].GetRank() == hand[3].GetRank() && hand[3].GetRank() == hand[4].GetRank())
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /* Check if the hand can be arranged so that the rank increases by 1 on each sequential card */
+        public Boolean isStraight()
+        {
+            SortHandByRank();
+            if (hand[0].GetValue() == hand[1].GetValue() + 1)
+            {
+                if (hand[1].GetValue() == hand[2].GetValue() + 1)
+                {
+                    if (hand[2].GetValue() == hand[3].GetValue() + 1)
+                    {
+                        if (hand[3].GetValue() == hand[4].GetValue() + 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        /* Check if the hand has a three of a kind and a two of a kind */
+        public Boolean FullHouse()
+        {
+            SortHandBySuit();
+            if (hand[0].GetRank() == hand[1].GetRank() && hand[1].GetRank() == hand[2].GetRank() &&
+                hand[3].GetRank() == hand[4].GetRank())
+            {
+                return true;
+            }
+            if (hand[0].GetRank() == hand[1].GetRank() && hand[2].GetRank() == hand[3].GetRank() &&
+                hand[3].GetRank() == hand[4].GetRank())
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /* Check if the hand has 3 cards of the same rank */
+        public Boolean ThreeOfAKind()
+        {
+
+            SortHandByRank();
+
+            if (hand[0].GetRank() == hand[1].GetRank() &&
+                 hand[1].GetRank() == hand[2].GetRank())
+            {
+                return true;
+            }
+
+            if (hand[1].GetRank() == hand[2].GetRank() &&
+                 hand[2].GetRank() == hand[3].GetRank())
+            {
+                return true;
+            }
+
+            if (hand[2].GetRank() == hand[3].GetRank() &&
+                 hand[3].GetRank() == hand[4].GetRank())
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        /* Check if the hand has two pairs of card with the same rank */
+        public Boolean TwoPairs()
+        {
+
+            SortHandByRank();
+
+            if (hand[0].GetRank() == hand[1].GetRank() &&
+                 hand[2].GetRank() == hand[3].GetRank())
+            {
+                return true;
+            }
+
+            if (hand[0].GetRank() == hand[1].GetRank() &&
+                 hand[3].GetRank() == hand[4].GetRank())
+            {
+                return true;
+            }
+
+            if (hand[1].GetRank() == hand[2].GetRank() &&
+                 hand[3].GetRank() == hand[4].GetRank())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        /* Check if hand has a pair of cards with the same rank */
+        public Boolean Pair()
+        {
+            SortHandByRank();
+
+            if (hand[0].GetRank() == hand[1].GetRank()) { return true; }
+
+            if (hand[1].GetRank() == hand[2].GetRank()) { return true; }
+
+            if (hand[2].GetRank() == hand[3].GetRank()) { return true; }
+
+            if (hand[3].GetRank() == hand[4].GetRank()) { return true; }
+
+            return false;
+        }
+
+
+        /* Get a card from the deck given its index */
+        public Card GetCardByIndex(int i)
+        {
+            return hand[i];
+        }
+
+
+        /* Evaluate the hand and determine the value of the hand */
+        public String ValidateHand()
+        {
+            if (Flush())
+            {
+                setHandValue(6);
+                return "Flush!";
+            }
+            else if (FourOfAKind())
+            {
+                setHandValue(5);
+                return "Four of a kind!";
+            }
+            else if (FullHouse())
+            {
+                setHandValue(4);
+                return "Full house!";
+            }
+            else if (ThreeOfAKind())
+            {
+                setHandValue(3);
+                return "Three of a kind!";
+            }
+            else if (TwoPairs())
+            {
+                setHandValue(2);
+                return "Two pairs!";
+            }
+            else if (Pair())
+            {
+                setHandValue(1);
+                return "One pair!";
+            }
+            else
+            {
+                setHandValue(0);
+                return "";
+            }
+        }
+
+
+        /* Set the value of the hand */
+        public void setHandValue(int val)
+        {
+            handValue = val;
+        }
+
+
+        /* Get the value of the hand */
+        public int getHandValue()
+        {
+            return handValue;
+        }
+    }
+}
